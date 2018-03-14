6894924cfc519c2204f627dd01c54dd93ab23306
John Mai <mai.john294@gmail.com>
2018-03-13 17:02:17 -0700

    Have buttons fully implemented and had them working at one point. Need to debug

--- RefereeBout/ViewController.swift
+++ RefereeBout/ViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //default settings to tinker with as bout progresses
     var scoreRight = 0
     var scoreLeft = 0
     var maxTouch = 5
     
     var boutIsOver = false
-    
+    var priority: Bool?
+    var whoHasPriority: Int?
     var timeStop = true
+    var period = 1
     
     //3 minutes = 180sec
     var time = 180
@@ -32,10 +35,15 @@ class ViewController: UIViewController {
     var blackCardRight = 0
     
     
+    @IBOutlet weak var leftScoreView: UILabel!
+    @IBOutlet weak var rightScoreView: UILabel!
+    
     @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        leftScoreView.text = String(0)
+        rightScoreView.text = String(0)
         timeLabel.text = String(timeCount)
         
     }
@@ -52,7 +60,15 @@ class ViewController: UIViewController {
         countTime()
     }
     
-    
+    func determinePriority() {
+        let randomNum = (Int)(arc4random() * 2)
+        //1 is equal to left, 0 is equal to right
+        if (randomNum == 1) {
+            whoHasPriority = 1
+        } else {
+            whoHasPriority = 0
+        }
+    }
     func countTime() {
         if (!timeStop && time > 0) {
             let minutes = String(time/60)
@@ -69,8 +85,14 @@ class ViewController: UIViewController {
     }
     
     func doubleTouch() {
+        if ((maxTouch == 5 && scoreLeft != 4 && scoreRight != 4 ) || (maxTouch == 15 && scoreLeft != 14 && scoreRight != 14)) {
+            scoreLeft += 1
+            scoreRight += 1
+        }
+    }
+    
+    func reset() {
         
     }
-
 }
 
